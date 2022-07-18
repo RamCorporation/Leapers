c2b068997c1aa89b46edff6134ef08664a8fcea4
RamGemes <779haydenk@gmail.com>
2022-07-18 18:09:30 -0400

    initial commit

---
+++ src/main/java/net/ramgaming/leapers/Leapers.java
@@ -0,0 +1,15 @@
+package net.ramgaming.leapers;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Leapers implements ModInitializer {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("leapers");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/ramgaming/leapers/LeapersClient.java
@@ -0,0 +1,10 @@
+package net.ramgaming.leapers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LeapersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
